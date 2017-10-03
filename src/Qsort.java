8d6a621855cc162e07782219ed7b86d8e3cb9a80
Adhish Thite <athite@uncc.edu>
2017-10-02 20:38:35 -0400

    Quick Sort implemented without File Handling.
    Taken randomized input (of 'Double' data type and performs Quick Sort on the data.
    
    The time required to quick sort the data is noted.

--- src/Qsort.java
+++ src/Qsort.java
@@ -5,6 +5,45 @@
  * Algorithms and Data Structures - Programming Assignment 2
  *
  **/
-public class Qsort {
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Qsort {
+    public static void main(String[] args) {
+
+        ArrayList<Double> randomList = new ArrayList<Double>();
+
+        int min = -9000;
+        int max = 1000;
+
+        System.out.println("\nProcessing Input Arrays\n\n");
+        for(int i = min; i <= max; i++) {
+            Random r = new Random();
+            randomList.add((min + (max - min) * r.nextDouble()));
+        }
+
+        Double[] arr = randomList.toArray(new Double[randomList.size()]);
+
+        QuickSort q = new QuickSort(arr, "test.txt");
+        QuickSort.SortData sd = q.getSortData();
+
+
+
+        if(validateSortedArray(arr)){
+            System.out.println("\nThe array has been successfully sorted !\n");
+        } else {
+            System.out.println("\nThere has been some error in sorting !\n");
+        }
+    }
+
+    private static boolean validateSortedArray(Double[] array) {
+        for(int i = 0; i < array.length - 1; i++){
+            if(array[i] > array[i+1]){
+                return  false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/QuickSort.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+
+/**
+ * Implementing 'Quick Sort' Algorithm
+ *
+ * Adhish Thite | athite@uncc.edu | UNCC Student ID : 801023459
+ * Algorithms and Data Structures - Programming Assignment 2
+ *
+ **/
+
+public class QuickSort {
+    private static Double[] arrayToSort;
+    public SortData sd;
+
+    public QuickSort() {
+        arrayToSort = null;
+    }
+
+    public QuickSort(Double[] arrayToSort_Param, String fileName) {
+        this.arrayToSort = arrayToSort_Param;
+
+        Long startTime = System.currentTimeMillis();
+        doQuickSort(arrayToSort,0,arrayToSort.length - 1);
+        Long elapsedTime = System.currentTimeMillis() - startTime;
+
+        sd = new SortData(arrayToSort, elapsedTime, fileName);
+    }
+
+    public void doQuickSort(Double[] array, int min, int max) {
+
+        if (min < max) {
+            int index = doPartition(array, min, max);
+
+            doQuickSort(array, min, index - 1);
+            doQuickSort(array, index + 1, max);
+        }
+    }
+
+    private int doPartition(Double[] tempArray, int min, int max ) {
+
+        Double pivot = tempArray[max];
+        int i = (min - 1);
+
+        for(int j = min; j <= max - 1; j++) {
+            if (tempArray[j] <= pivot) {
+                i++;
+
+                Double temp = tempArray[i];
+                tempArray[i] = tempArray[j];
+                tempArray[j] = temp;
+            }
+        }
+
+        Double temp2 = tempArray[i+1];
+        tempArray[i+1] = tempArray[max];
+        tempArray[max] = temp2;
+
+        return (i + 1);
+    }
+
+    public class SortData{
+        Double[] sortedArray;
+        Long elapsedTime;
+        String fileName;
+
+        public SortData(Double[] sortedArray, Long elapsedTime, String fileName) {
+            this.sortedArray = sortedArray;
+            this.elapsedTime = elapsedTime;
+            this.fileName = fileName;
+
+            System.out.println("Input File >:\t" + fileName);
+            System.out.println("Array Length >:\t" + sortedArray.length);
+            System.out.println("\nSorted Array is -\n" + Arrays.toString(sortedArray));
+            System.out.println("\nArray has been sorted in >:\t" + elapsedTime + " ms");
+        }
+    }
+
+    public SortData getSortData(){
+        return sd;
+    }
+}
